5ed0fb743a430884a8d3c280789cc4ab1fab42bf
garrynewman <garrynewman@82b74e7c-beea-4a93-d681-cf472943600a>
2012-02-07 12:58:01 +0000

    Patch from issue 76
    git-svn-id: http://gwen.googlecode.com/svn/trunk/trunk@191 82b74e7c-beea-4a93-d681-cf472943600a

--- gwen/Projects/inc/linux.lua
+++ gwen/Projects/inc/linux.lua
@@ -1,7 +1,7 @@
 
 
-ALLEGRO_LIBS	= { "Renderer-Allegro", "UnitTest", "GWEN Static", "allegro", "allegro_image", "allegro_font", "allegro_ttf", "allegro_primitives"}
-ALLEGRO_LIBS_D	= { "Renderer-Allegro", "UnitTest", "GWEN Static", "allegro", "allegro_image", "allegro_font", "allegro_ttf", "allegro_primitives"}
+ALLEGRO_LIBS	= { "UnitTest", "Renderer-Allegro", "GWEN Static", "alleg" }
+ALLEGRO_LIBS_D	= { "UnitTest", "Renderer-Allegro", "GWEN Static", "alleg" }
 
-SFML_LIBS		= { "Renderer-SFML", "GL", "GWEN Static", "UnitTest", "sfml-window", "sfml-graphics", "sfml-system" }
-SFML_LIBS_D		= { "Renderer-SFML", "GL", "GWEN Static", "UnitTest", "sfml-window", "sfml-graphics", "sfml-system" }
+SFML_LIBS		= { "UnitTest", "Renderer-SFML", "GL", "GWEN Static", "sfml-window", "sfml-graphics", "sfml-system" }
+SFML_LIBS_D		= { "UnitTest", "Renderer-SFML", "GL", "GWEN Static", "sfml-window", "sfml-graphics", "sfml-system" }
