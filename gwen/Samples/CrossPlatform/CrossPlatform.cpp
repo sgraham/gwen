e6823d6635732ac077658766e97c6c74da9da563
garrynewman <garrynewman@82b74e7c-beea-4a93-d681-cf472943600a>
2012-02-22 20:47:22 +0000

    Oops, didn't mean to commit this
    git-svn-id: http://gwen.googlecode.com/svn/trunk/trunk@208 82b74e7c-beea-4a93-d681-cf472943600a

--- gwen/Samples/CrossPlatform/CrossPlatform.cpp
+++ gwen/Samples/CrossPlatform/CrossPlatform.cpp
@@ -9,10 +9,10 @@
 // These are the renderers that work with WindowCanvas
 // (you need to include the relevant libs to use them)
 //
-//#include "Gwen/Renderers/OpenGL_DebugFont.h"
+#include "Gwen/Renderers/OpenGL_DebugFont.h"
 //#include "Gwen/Renderers/GDIPlus.h"
 //#include "Gwen/Renderers/Direct2D.h"
-#include "Gwen/Renderers/DirectX9.h"
+//#include "Gwen/Renderers/DirectX9.h"
 
 
 //
@@ -30,8 +30,8 @@ int main()
 	//
 	//Gwen::Renderer::GDIPlusBuffered		renderer;
 	//Gwen::Renderer::Direct2D				renderer;
-	Gwen::Renderer::DirectX9				renderer;
-	//Gwen::Renderer::OpenGL_DebugFont		renderer;
+	//Gwen::Renderer::DirectX9				renderer;
+	Gwen::Renderer::OpenGL_DebugFont		renderer;
 	Gwen::Skin::TexturedBase			skin( &renderer );
 
 	//
